7642b8f3f402da1c2b610bd69f58aaa0fc38b9b3
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-31 23:37:44 +0200

    feat: add ingredient data class

--- src/main/kotlin/webscraper/model/Ingredient.kt
+++ src/main/kotlin/webscraper/model/Ingredient.kt
@@ -1,3 +1,13 @@
 package webscraper.model
 
-data class Ingredient()
+/**
+ * Data class for Ingredients
+ * @author Pascal Kühnold
+ *
+ * @property amount the ingredient amount
+ * @property name the name of the ingredient
+ */
+data class Ingredient(
+    var amount: String = "",
+    var name: String = ""
+)
