88ab5bd9d1177d83248fb89c87bb5b7327a9943c
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-31 23:37:30 +0200

    feat: add main method with a start url

--- src/main/kotlin/MainApp.kt
+++ src/main/kotlin/MainApp.kt
@@ -1,2 +1,10 @@
-class MainApp {
+import webscraper.ChefkochMealScraper
+
+
+fun main(){
+    println(
+        ChefkochMealScraper("https://www.chefkoch.de/rezepte/1769391286881789/Zucchini-Omelett-mit-Kaese.html")
+        .getExtractedMealData()
+        .toString()
+    )
 }
